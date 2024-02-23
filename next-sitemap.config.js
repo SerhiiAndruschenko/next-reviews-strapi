50911b8eb41f5e1d55703ca40ffae10ef380f083
SerhiiAndruschenko <andruschenko033@gmail.com>
2024-02-23 20:45:46 +0200

    noindex

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://www.bookwormhaven.space/',
-    generateRobotsTxt: true, 
-  }
+  siteUrl: process.env.SITE_URL || "https://next-reviews-strapi.onrender.com/",
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    // Customize the rules for robots.txt
+    policies: [
+      {
+        userAgent: "*", // This applies to all web crawlers
+        disallow: ["/"], // Disallow indexing of all pages
+      },
+    ],
+  },
+};
